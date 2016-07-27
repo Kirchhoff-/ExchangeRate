829c2898bc47997ea062265c8c71c995ce320b69
Kirchhoff- <kirchhoff.19.92@gmail.com>
2016-07-27 22:14:43 +0300

    Add layouts files

--- app/src/main/java/com/kirchhoff/exchangerates/currencylist/CurrencyListContract.java
+++ app/src/main/java/com/kirchhoff/exchangerates/currencylist/CurrencyListContract.java
@@ -1,7 +1,13 @@
 package com.kirchhoff.exchangerates.currencylist;
 
+import com.kirchhoff.exchangerates.BasePresenter;
+import com.kirchhoff.exchangerates.BaseView;
+import com.kirchhoff.exchangerates.CurrencyItem;
+
+import java.util.List;
+
 /**
  * @author Kirchhoff-
  */
-public class CurrencyListContract {
+public interface CurrencyListContract {
 }
